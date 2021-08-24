62e7ed94011527ebaadc87031580bf3cad039e38
SAKotov <mozz.kotov@gmail.com>
2021-08-24 23:58:12 +0300

    fifth lesson completed

--- lesson/lesson_2.php
+++ lesson/lesson_2.php
@@ -19,20 +19,20 @@
 //
 //Не подсматривая в мой код реализуйте такой же класс User с методом show.
 
-class User
-{
-    public $name;
-    public $age;
-
-    public function show($str)
-    {
-        return $str .'!!!';
-    }
-}
-
-$user = new User();
-
-$user->name = 'Вася';
-$user->age = '25';
-
-echo $user->show('Hello ');
+//class User
+//{
+//    public $name;
+//    public $age;
+//
+//    public function show($str)
+//    {
+//        return $str .'!!!';
+//    }
+//}
+//
+//$user = new User();
+//
+//$user->name = 'Вася';
+//$user->age = '25';
+//
+//echo $user->show('Hello ');
