6979fbc989e9c0c6b38bae94b4b3da5776f0c885
SAKotov <mozz.kotov@gmail.com>
2021-08-26 18:41:57 +0300

    lesson 12 (Initial property values when declared)

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
 //include 'lesson/lesson_8.php';
 //include 'lesson/lesson_9.php';
 //include 'lesson/lesson_10.php';
-include 'lesson/lesson_11.php';
-//include 'lesson/lesson_12.php';
+//include 'lesson/lesson_11.php';
+include 'lesson/lesson_12.php';
 //include 'lesson/lesson_13.php';
 //include 'lesson/lesson_14.php';
 //include 'lesson/lesson_15.php';
