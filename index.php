6556bcf8cf83faccc4b43f100d0c533e8c53c1bf
SAKotov <mozz.kotov@gmail.com>
2021-08-28 19:46:27 +0300

    lesson 19 (Passing objects as parameters)

--- index.php
+++ index.php
@@ -16,4 +16,7 @@
 //include 'lesson/lesson_15/lesson.php';
 //include 'lesson/lesson_16/lesson.php';
 //include 'lesson/lesson_17/lesson.php';
-include 'lesson/lesson_18/lesson.php';
+//include 'lesson/lesson_18/lesson.php';
+include 'lesson/lesson_19/lesson.php';
+
+
